f71fae5b9abaa6579807ad3c498ef0800938e60e
M3116072 <42989467+M3116072@users.noreply.github.com>
2018-12-28 14:20:37 +0700

    by kiki

---
+++ app/src/main/java/com/example/amarell/tubesandroid/Login.java
@@ -0,0 +1,13 @@
+package com.example.amarell.tubesandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
