08f7ab1cb1f4ff7f4b32ffef4efc95c58a464933
M3116072 <42989467+M3116072@users.noreply.github.com>
2018-12-28 15:17:53 +0700

    by kiki

--- app/src/main/java/com/example/amarell/tubesandroid/Login.java
+++ app/src/main/java/com/example/amarell/tubesandroid/Login.java
@@ -1,13 +1,94 @@
 package com.example.amarell.tubesandroid;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity implements View.OnClickListener{
+    FirebaseAuth mAuth;
+    EditText EtEmail, EtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        mAuth= FirebaseAuth.getInstance();
+        EtEmail =(EditText) findViewById(R.id.username);
+        EtPassword=(EditText) findViewById(R.id.password);
+
+        findViewById(R.id.login).setOnClickListener(this);
+        findViewById(R.id.TvSignUp).setOnClickListener(this);
+
+}
+
+    private void userlogin() {
+        String email = EtEmail.getText().toString().trim();
+        String password= EtPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            EtEmail.setError("Email harus Diisi");
+            EtEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            EtEmail.setError("Masukan Email Yang Valid");
+            EtEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            EtPassword.setError("Password harus diisi");
+            EtPassword.requestFocus();
+            return;
+        }
+        if (password.length()<6){
+            EtPassword.setError("Password minimal 6 karakter");
+            EtPassword.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+
+                    Intent intent = new Intent(Login.this, MainActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }else {
+                    Toast.makeText(getApplicationContext(),task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.TvSignUp:
+
+                startActivity(new Intent(this, Register.class));
+                break;
+
+            case R.id.login:
+                userlogin();
+                break;
+        }
+    }
+
+
 }
---
+++ app/src/main/java/com/example/amarell/tubesandroid/Register.java
@@ -0,0 +1,98 @@
+package com.example.amarell.tubesandroid;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+
+public class Register extends AppCompatActivity implements View.OnClickListener {
+    EditText EtEmail, EtPassword;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        EtEmail=(EditText) findViewById(R.id.username);
+        EtPassword=(EditText) findViewById(R.id.password);
+
+        findViewById(R.id.TvSignIn).setOnClickListener(this);
+        findViewById(R.id.register).setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void registeruser(){
+        String email = EtEmail.getText().toString().trim();
+        String password= EtPassword.getText().toString().trim();
+
+        if (email.isEmpty()){
+            EtEmail.setError("Email harus diisi");
+            EtEmail.requestFocus();
+            return;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            EtEmail.setError("Masukan Email Yang Valid");
+            EtEmail.requestFocus();
+            return;
+        }
+
+        if (password.isEmpty()){
+            EtPassword.setError("Password harus diisi");
+            EtPassword.requestFocus();
+            return;
+        }
+
+        if (password.length()<6){
+            EtPassword.setError("Password minimal 6 karakter");
+            EtPassword.requestFocus();
+            return;
+        }
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "User Berhasil Registrasi", Toast.LENGTH_SHORT).show();
+                        }else{
+                            if (task.getException() instanceof FirebaseAuthUserCollisionException){
+                                Toast.makeText(getApplicationContext(), "User telah terdaftar", Toast.LENGTH_SHORT).show();
+
+                            }else{
+                                Toast.makeText(getApplicationContext(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+
+                            }
+
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                registeruser();
+                break;
+
+            case R.id.TvSignIn:
+                startActivity(new Intent(this, Login.class));
+        }
+
+    }
+}
