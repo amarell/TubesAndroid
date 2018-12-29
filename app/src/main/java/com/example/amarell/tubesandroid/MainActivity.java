6250d40c94912dc15c3ad7e11da0f2ab5af6240d
M3116072 <42989467+M3116072@users.noreply.github.com>
2018-12-29 13:29:25 +0700

    by kiki

--- app/src/main/java/com/example/amarell/tubesandroid/MainActivity.java
+++ app/src/main/java/com/example/amarell/tubesandroid/MainActivity.java
@@ -6,9 +6,11 @@ import android.support.design.widget.Snackbar;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.SearchView;
 
 import com.example.amarell.tubesandroid.Tab.MyAdapter;
 import com.example.amarell.tubesandroid.Tab.SlidingTabLayout;
@@ -47,7 +49,22 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        android.widget.SearchView searchView =(android.widget.SearchView)item.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                //mViewPager.getFilter().filter(s);
+                return false;
+            }
+        });
         return true;
     }
 
